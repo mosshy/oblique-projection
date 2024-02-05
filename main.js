f08d4aa88e43d25fbcb1f0ae11a6c7bc325ae1c6
unknown <mosshy1030@gmail.com>
2024-02-05 21:08:43 +0900

    draw ball

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 100, 100);
+
+ctx.beginPath();
+ctx.arc(75, 75, 10, 0, Math.PI * 2);
+ctx.stroke();
